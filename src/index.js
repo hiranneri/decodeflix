2ff1b2dc8b4918b1c0ce1c87526482aba67da90c
hiranneri <hiran.souza93@gmail.com>
2021-07-26 02:57:42 -0300

    adding configs for production

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 const app = require('./server')
-const port = 8080;
+const port = 8080 || process.env.PORT;
 
 app.listen(port, ()=> console.log(`Server is running at ${port}`));
--- src/routes/filmes/filmes.test.js
+++
@@ -1,4 +0,0 @@
-const request = require('supertest')
-const app = require('../../server')
-const idUsuario = 'af79607c-a867-4190-958f-3b236583b8c4';
-const token = process.env.TOKEN
